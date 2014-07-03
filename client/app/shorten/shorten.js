460f41db81d5b01b7a6a177325088e3fb090d8e2
Hack Reactor Students <xianhui.feng100@gmail.com>
2014-07-02 20:32:16 -0700

    Fixed link submissions.

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -3,7 +3,7 @@ angular.module('shortly.shorten', [])
 .controller('ShortenController', function ($scope, $location, Links) {
   $scope.link = {};
   $scope.addLink = function () {
-    Links.addLink($scope.link.url).then(function (resp) {
+    Links.addLink($scope.link).then(function (resp) {
       $scope.loading = false;
     }).catch(function (error) {
       console.error(error);
